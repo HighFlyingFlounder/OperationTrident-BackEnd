039f3736ca19a5a36b38ca9cc50e3fe6f62d89ed
w2w <904192625@qq.com>
2018-06-14 16:56:00 +0800

    Add run.sh and createGameTable.sql

---
+++ createGameTable.sql
@@ -0,0 +1,3 @@
+create table user(id VARCHAR(50), pw TEXT, PRIMARY KEY(id) )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table player(id VARCHAR(50),data BLOB,PRIMARY KEY(id) )ENGINE=InnoDB DEFAULT CHARSET=utf8; 
---
+++ run.sh
@@ -0,0 +1,1 @@
+mono OperationTridentBackEnd/bin/Debug/OperationTridentBackEnd.exe
